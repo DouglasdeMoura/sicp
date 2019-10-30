2e55835c858f9d02932b6d0be1d5f35750dab739
Douglas Moura <douglas.ademoura@gmail.com>
2019-10-30 18:38:08 -0300

    answer: Exercise 1.4

---
+++ ex-1.4.scm
@@ -0,0 +1,9 @@
+; EXERCISE 1.4: Observe that our model of evaluation allows for
+; combinations whose operators are compound expressions.
+; Use this observation to describe the behavior of the following procedure:
+
+(define (a-plus-abs-b a b)
+  ((if (> b 0) + -) a b))
+
+; The if-statement returns a primitive procedure (either + or -)
+; to evaluate a and b, depending on b relation with 0.

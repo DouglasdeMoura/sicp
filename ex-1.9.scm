8fba15c483cb9c157f971dc6f34a8f76d79cd429
Douglas Moura <douglas.ademoura@gmail.com>
2019-11-01 15:30:06 -0300

    answer: Exercise 1.9

--- ex-1.9.scm
+++ ex-1.9.scm
@@ -25,4 +25,26 @@ iterative or recursive?
 #|
 Let's evaluate the first procedure:
 
+(+ 4 5)
+(inc (+ 3 5))
+(inc (inc (+ 2 5)))
+(inc (inc (inc (+ 1 5))))
+(inc (inc (inc (inc (+ 0 5)))))
+(inc (inc (inc (inc (5)))))
+(inc (inc (inc (6))))
+(inc (inc (7)))
+(inc 8)
+9
+
+=> The first procedure is RECURSIVE.
+
+The second procedure:
+(+ 4 5)
+(+ 3 6)
+(+ 2 7)
+(+ 1 8)
+(+ 0 9)
+9
+
+=> The second procedure is ITERATIVE.
 |#

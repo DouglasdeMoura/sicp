111e067a5f7c222447b45190ad409d568ce918ee
Douglas Moura <douglas.ademoura@gmail.com>
2019-11-05 18:45:06 -0300

    answer: Exercise 1.13

---
+++ ex-1.13.scm
@@ -0,0 +1,8 @@
+#|
+EXERCISE 1.13: Prove that Fib(n) is the closest integer to (φ^n)/sqrt(5), where φ = (1+sqrt(5))/2
+ Hint: Let ψ=(1−sqrt(5))/2. Use induction and the definition of the Fibonacci numbers (see 1.2.2)
+ to prove that Fib(n)=(φ^n−ψ^n)/sqrt(5).
+|#
+
+; -----| ANSWER |-----
+; This one I made by hand
